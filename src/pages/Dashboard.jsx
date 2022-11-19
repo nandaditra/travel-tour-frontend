b40c6c5e9b0fc52b13de6fb54f070b07b6305bd3
Nanda Aditya Putra <nandaditra56@gmail.com>
2022-11-19 10:40:14 +0700

    Not add something in here

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,30 +1,43 @@
+import { useState } from "react" 
 import HeaderPage from "../components/HeaderPage"
 import FooterPage from "../components/FooterPage"
+import NotFoundPage from "./NotFoundPage"
 import {Packet} from  "./Packet"
 import { datas } from "./apidatas"
 import "../index.css"
 
 export default function Dashboard() {
-    return (
-        <div >
-           <HeaderPage />
-            <main className="main-dashboard-pages">
-               <div className="container-fluid"> 
-                <div className="row mx-5">
-                    {datas.map((item, index)=>
-                          <Packet 
-                             key={index}
-                             packet = {item} 
-                          />
-                    )}
+    const [search, setSearch] = useState()
+    
+    const findData = datas.find((data) => data.name === datas)
+
+    // const seacrhQuery = () => {
+    //     setSearch()
+    // }
+
+    if(search !== findData) {
+        return <NotFoundPage />
+    } else {
+        return (
+            <div >
+            <HeaderPage />
+                <main className="main-dashboard-pages">
+                <div className="container-fluid"> 
+                    <div className="row mx-5">
+                        {datas.map((item, index)=>
+                            <Packet 
+                                key={index}
+                                packet = {item} 
+                            />
+                        )}
+                    </div>
                 </div>
-              </div>
-            </main>
-           <FooterPage />
-        </div>
-    )
+                </main>
+            <FooterPage />
+            </div>
+      )
+  }
+
+
 }
-
-
-
 
