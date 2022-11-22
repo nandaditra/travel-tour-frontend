358aff4b798788218eeb58d8107a9e8a385b0bf8
Nanda Aditya Putra <nandaditra56@gmail.com>
2022-11-22 12:59:16 +0700

    add some features

--- src/pages/OrderPacket.jsx
+++ src/pages/OrderPacket.jsx
@@ -1,15 +1,54 @@
+import React from 'react'
+import { useState } from 'react'
+import FooterPage from '../components/FooterPage'
 import '../index.css'
-import { Button, Form } from 'react-bootstrap'
 
 export default function OrderPacket() {
+    const [name, setName] = useState() 
+    const [date, setDate] = useState()
+    const [person, setPerson] = useState()
+    const [country, setCountry] = useState()
+    const [province, setProvince] = useState()
+    const [Regions, seRegions] = useState()
+    const [phoneNumber, setPhoneNumber] = useState()
+    const [address, setAddress] = useState()
+
     return (
-            <Form className="bg-white">
-               <Form.Group className="mb-3" controlId="formBasicEmail">
-                 <Form.Label>Your Name</Form.Label>
-                 <></>
-               </Form.Group>
-               <Button>Cancel</Button>
-               <Button>Book</Button>
-            </Form>
+            <div className="bg-white p-5">
+               <div className="row m-5 text-start">
+                   <div className="col-6">
+                      <form action="/" method="post">
+                         <div className="mb-3 col-12">
+                             <label className="form-label">Your Name</label>
+                             <input name="name" className="form-control" placeholder='Your Name' />
+                         </div>
+                         <div className="mb-3 col-12">
+                             <label className="form-label">Date</label>
+                             <input name="date" className="form-control" placeholder='20 September' />
+                         </div>
+                         <div className="mb-3 col-12">
+                             <label className="form-label">Person</label>
+                             <input name="person" className="form-control" placeholder='0'/>
+                         </div>      
+                         <div className="mb-3 col-5">
+                              <label className="form-label">Country</label>
+                              <input name="country" className="form-control" placeholder='Ex: Malaysia'/>
+                         </div>
+                         <div className="mb-3 col-5">
+                              <label className="form-label">Province</label>
+                               <input name="province" className="form-control" placeholder='Jakarta'/>
+                         </div>
+                         <div>
+                              
+                         </div>
+                      </form>
+                   </div>
+                   <div className="col-6">
+
+                   </div>
+               </div>
+            
+               <FooterPage />
+            </div>
     )
 }
